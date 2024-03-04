39afbc568b0b5a959fdec2057f55d7b04d6b2269
Natalia Ivantsova <ivantsovana@gmail.com>
2024-03-04 21:02:53 +0200

    feat: add vignerecipheringmachine func solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directMachine = true) {
+    this.directMachine = directMachine;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(string, key) {
+    return this.do(string, key, 'encrypt');
+  }
+  decrypt(string, key) {
+    return this.do(string, key, 'decrypt');
+  }
+
+  do(string, key, type) {
+    if (!string || !key || !type) {
+      throw new Error('Incorrect arguments!')
+    };
+
+    let j = -1;
+    const upKey = key.toUpperCase();
+    const charArr = string.toUpperCase().split('').map((c) => {
+      const chcode = c.charCodeAt();
+
+      if (chcode >= 65 && chcode <= 90) {
+        j++;
+        if (type === 'encrypt')
+          return String.fromCharCode(
+            (chcode + upKey[(j) % upKey.length].charCodeAt() - 130) % 26 + 65
+          );
+        if (type === 'decrypt') {
+          return String.fromCharCode(
+            (chcode - 65 + (26 - upKey[(j) % upKey.length].charCodeAt() + 65)) % 26 + 65
+          );
+        }
+      }
+      return c;
+    });
+
+    return this.directMachine
+      ? charArr.join('')
+      : charArr.reverse().join('');
   }
 }
 
